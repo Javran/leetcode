6dba88526fdd7489e6a377e7d2ed939914c9735f
Javran Cheng <Javran.c@gmail.com>
2018-05-08 12:47:05 -0400

    fix build

--- run/prepare.sh
+++ run/prepare.sh
@@ -7,14 +7,14 @@ GTEST_AR=gtest-1.7.0.zip
 
 # download archive
 if [ ! -f ${GTEST_AR} ]; then
-    curl "https://googletest.googlecode.com/files/${GTEST_AR}" -o "${GTEST_AR}" -L
+    curl "https://github.com/google/googletest/archive/release-1.7.0.zip" -o $GTEST_AR -L
 fi
 
 # change dir name to a constant name
 unzip -n ${GTEST_AR}
-mv ${GTEST_AR/.zip/} gtest-home
+mv googletest-release-1.7.0 gtest-home
 cd gtest-home
 
 # build gtest
-./configure
+cmake .
 make
