f92f56946d01cc69bbc82ea57c257f245b10774b
Javran Cheng <Javran.c@gmail.com>
2018-05-21 00:48:28 -0400

    sol: letter comb
    messy but working.

--- letter-combinations-of-a-phone-number/Solution.cs
+++ letter-combinations-of-a-phone-number/Solution.cs
@@ -14,7 +14,38 @@ public class Solution {
         return MakeLetters(s).Select(x => Int32.Parse(x)).ToList();
     }
 
+    static List<List<string>> Sequence(List<List<string>> xs) {
+        if (xs.Count() == 0) {
+            return new List<List<string>>{new List<string>{} };
+        } else {
+            var y = xs.First();
+            var ys = xs.Skip(1);
+            return y.SelectMany(yResult => Sequence(ys.ToList()).SelectMany(ysResult => {
+                        List<string> result = new List<string>(ysResult.Count() + 1);
+                        result.Add(yResult);
+                        foreach (var ysR in ysResult)
+                            result.Add(ysR);
+                        return new List<List<string>>{result};
+                    })).ToList();
+        }
+    }
+
     public IList<string> LetterCombinations(string digitsRaw) {
+        /*
+
+          the problem setter apparently says nothing about what to do
+          if the input is empty.
+
+          An empty string can be interpreted as:
+          - "no input at all" (like null), in which case {} should be the answer.
+          - or "input is an empty string", in which case {""} should be used.
+
+          this decision is so random that getting this right first time is pure luck.
+          
+        */
+        if (digitsRaw == "")
+            return new List<string>{};
+
         var digitToLetters = new Dictionary<int, List<string>>
         {
             { 2, MakeLetters("abc") },
@@ -27,14 +58,17 @@ public class Solution {
             { 9, MakeLetters("wxyz") },
         };
 
-        var digits = GetDigits(digitsRaw);
-
-        // TODO
-        return new List<string>();
+        var letters = GetDigits(digitsRaw).Select(d => digitToLetters[d]).ToList();
+        var results = Sequence(letters);
+        return results.Select(xs => String.Join("", xs)).ToList();
     }
 
     static void Main(string[] args) {
-        // var s = new Solution();
+        var s = new Solution();
+        var result = s.LetterCombinations("23");
+        foreach (var r in result) {
+            Console.WriteLine(r);
+        }
     }
 
 }
