36550a71d50504eed70f5a706a23cf6c8bf0321a
Javran Cheng <Javran.c@gmail.com>
2018-05-23 22:26:30 -0400

    slightly improved performance

--- 3sum/solution.js
+++ 3sum/solution.js
@@ -1,48 +1,101 @@
+// for performing binary search in a sorted array
+const binarySearch = (xs, target, loIndIn, hiIndIn) => {
+  let loInd = loIndIn, hiInd = hiIndIn
+
+  while (loInd <= hiInd) {
+    const midInd = Math.floor((loInd+hiInd) / 2)
+    if (xs[midInd] === target) {
+      return midInd
+    } else if (xs[midInd] < target) {
+      loInd = midInd+1
+    } else {
+      // xs[midInd] > target
+      hiInd = midInd-1
+    }
+  }
+
+  return null
+}
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
-const threeSum = nums => {
+const threeSum = numsInp => {
+  // we would prefer not to mutate the input if possible
+  let nums = [...numsInp]
+  // sorting gives nice invariants that we can use
   nums.sort((x,y) => x - y)
-  const newNums = []
-  for (let i = 0; i < nums.length; ++i) {
-    if (newNums.length < 3 || nums[i] !== newNums[newNums.length-3])
-      newNums.push(nums[i])
-  }
-  nums = newNums
 
-  const selected = []
-  const pairs = new Map()
-  const pairFound = (a,b) => pairs.set(`${a},${b}`,[a,b,-a-b])
-  const search = beginInd => {
-    if (selected.length == 2) {
-      if (beginInd >= nums.length)
-        return
-      const target = -selected[0]-selected[1]
-      let loInd = beginInd, hiInd = nums.length-1
-      while (loInd <= hiInd) {
-        let midInd = Math.floor((hiInd+loInd) / 2)
-        if (nums[midInd] == target) {
-          pairFound(selected[0],selected[1])
-          return
-        } else if (nums[midInd] < target) {
-          loInd = midInd + 1
-        } else {
-          // nums[midInd] > target
-          hiInd = midInd - 1
-        }
-      }
-    } else {
-      for (let i = beginInd; i < nums.length; ++i) {
-        selected.push(nums[i])
-        search(i+1)
-        selected.pop()
+  const pairs = []
+  const pairFound = (a,b) => pairs.push([a,b,-a-b])
+
+  /*
+
+     to get a+b+c=0 for some a,b,c we have 3 cases:
+     - (1) a,b,c are all the same number: the only solution to a+a+a = 0 is a = 0,
+       so when we found 0, we simply add 0 into the list
+
+     - (2) a,b,c are 3 unique numbers
+
+     - (3) a,b,c has two duplicates
+
+   */
+  // (1) looking for 3 zeros (as it is sorted, it has to be consecutive)
+  let zInd = binarySearch(nums,0,0,nums.length-1)
+  // find first 0
+  while (zInd !== null && zInd-1 >= 0 && nums[zInd-1] === 0)
+    --zInd;
+  if (zInd !== null && zInd+2 < nums.length && nums[zInd+2] === 0) {
+    pairFound(0,0)
+  }
+
+  const uniqNums = []
+  const dups = []
+
+  for (let i = 0; i < nums.length; ++i) {
+    const num = nums[i]
+    if (uniqNums.length > 0 && uniqNums[uniqNums.length-1] === num) {
+      // we have seen this number before.
+      if (num !== 0 && (dups.length === 0 || dups[dups.length-1] !== num))
+        dups.push(num)
+    }
+    if (uniqNums.length === 0 || uniqNums[uniqNums.length-1] !== num)
+      uniqNums.push(num)
+  }
+
+  // (2) looking for a+b+c=0 where a != b, b != c, c != a
+  for (let i = 0; i < uniqNums.length; ++i) {
+    const a = uniqNums[i]
+    for (let j = i+1; j < uniqNums.length; ++j) {
+      const b = uniqNums[j]
+      const c = -a-b
+      if (c === a || c === b)
+        continue
+      if (binarySearch(uniqNums, c,j+1, uniqNums.length-1) !== null)
+        pairFound(a,b)
+    }
+  }
+
+  // (3) duplicated nums
+  for (let i = 0; i < dups.length; ++i) {
+    const num = dups[i]
+    const c = -num-num
+    if (binarySearch(uniqNums, c, 0, uniqNums.length-1) !== null) {
+      if (c > num) {
+        // num = num < c
+        pairFound(num, num)
+      } else {
+        // c < num = num
+        pairFound(c, num)
       }
     }
   }
-  search(0)
-  return [...pairs.values()]
+
+  return pairs
 }
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4]))
-console.log(threeSum([-1,-1,-1,-1, 1,1,1,1,1,3,4,4,4,4,4,5,5,5,5,7,7,8,9,9]))
+console.log(threeSum([-1, 0, 1, 2, -1,0, -4,0,0]))
+console.log(threeSum([-1,-1,-1,-1, 1,1,1,1,1,3,4,4,4,4,4,5,5,5,5,-2,-2,7,7,8,9,9,-5]))
+console.log(threeSum([0,0,0,0,0,0,0,0]))
