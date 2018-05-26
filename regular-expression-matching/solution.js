ceb56ee3946ff3a1cb8c80211f716c873dc5e473
Javran Cheng <Javran.c@gmail.com>
2018-05-26 00:38:22 -0400

    sol: re

--- regular-expression-matching/solution.js
+++ regular-expression-matching/solution.js
@@ -17,7 +17,11 @@ const buildFSA = pattern => {
       transitions.set(fromState, new Map())
     }
     const subMap = transitions.get(fromState)
-    subMap.set(echar, toState)
+    if (subMap.has(echar)) {
+      subMap.get(echar).add(toState)
+    } else {
+      subMap.set(echar, new Set([toState]))
+    }
   }
 
   const pats = pattern.split('')
@@ -41,9 +45,59 @@ const buildFSA = pattern => {
   const acceptingState = mkState()
   mkTr(prevState, 'EPS', acceptingState)
 
+  // direct lookup
+  const lookup = (state, echar) => {
+    if (transitions.has(state)) {
+      const subMap = transitions.get(state)
+      if (subMap.has(echar))
+        return subMap.get(echar)
+      else
+        return null
+    } else {
+      return null
+    }
+  }
+
+  const eClosure = stateSet => {
+    const newStates = new Set()
+    stateSet.forEach(state => {
+      const nexts = lookup(state, 'EPS')
+      if (nexts !== null)
+        nexts.forEach(next =>
+          !stateSet.has(next) && newStates.add(next)
+        )
+    })
+    if (newStates.size === 0) {
+      return stateSet
+    } else {
+      newStates.forEach(state => stateSet.add(state))
+      return eClosure(stateSet)
+    }
+  }
+
+  const nextStates = (stateSetInp, char) => {
+    const stateSet = eClosure(stateSetInp)
+    const endStateSet = new Set()
+    stateSet.forEach(state => {
+      const s1 = lookup(state, '.')
+      if (s1)
+        s1.forEach(st =>
+          endStateSet.add(st)
+        )
+      const s2 = lookup(state, char)
+      if (s2)
+        s2.forEach(st =>
+          endStateSet.add(st)
+        )
+    })
+    return eClosure(endStateSet)
+  }
+
+  const startStates = eClosure(new Set([startState]))
+
   return {
-    startState,
-    transitions,
+    startStates,
+    nextStates,
     acceptingState,
   }
 }
@@ -53,9 +107,20 @@ const buildFSA = pattern => {
  * @param {string} p
  * @return {boolean}
  */
-const isMatch = (s, pattern) => {
-  
+const isMatch = (str, pattern) => {
+  const fsa = buildFSA(pattern)
+  let curStates = new Set(fsa.startStates)
+  for (let i = 0; i < str.length; ++i) {
+    curStates = fsa.nextStates(curStates, str[i])
+  }
+  return curStates.has(fsa.acceptingState)
 }
 
-
-console.log(buildFSA('a*bc.*d'))
+console.log(isMatch("aa", "a") === false)
+console.log(isMatch("aa", "a*") === true)
+console.log(isMatch("ab", ".*") === true)
+console.log(isMatch("aab", "c*a*b") === true)
+console.log(isMatch("mississipi", "mis*is*p*.") === false)
+console.log(isMatch("mississipi", "mis*is*.p*.") === true)
+console.log(isMatch("aaa", "a*a") === true)
+console.log(isMatch("", ".*") === true)
