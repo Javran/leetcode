90caed8c257edc7c50e618488899d7fe885419bf
Javran Cheng <Javran.c@gmail.com>
2018-05-26 22:52:55 -0400

    various templates

---
+++ templates/Solution.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Solution {
+    static void Main(string[] args) {
+        Console.WriteLine("placeholder");
+    }
+}
---
+++ templates/Solution.kt
@@ -0,0 +1,8 @@
+class Solution {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("placeholder")
+        }
+    }
+}
