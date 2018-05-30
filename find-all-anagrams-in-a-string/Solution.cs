440243a2aaec841bc92376e2c28d04ec17a1b662
Javran Cheng <Javran.c@gmail.com>
2018-05-30 00:20:26 -0400

    further simplify

--- find-all-anagrams-in-a-string/Solution.cs
+++ find-all-anagrams-in-a-string/Solution.cs
@@ -2,64 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-/*
-  TyDigest is a type for digest of interest:
-  a char is contained if and only if the char presents in the pattern.
-*/
-using TyDigest = System.Collections.Generic.Dictionary<char, int>;
-
+// Given that all inputs are from chars of 'a' ~ 'z',
+// we have such a very small set that we can use array to our advantage.
 public class Solution {
-    public static bool Matches(TyDigest d, TyDigest pat) {
-        foreach (var k in pat.Keys) {
-            if (!d.ContainsKey(k) || d[k] != pat[k])
-                return false;
-        }
-        return true;
-    }
-
     public IList<int> FindAnagrams(string s, string p) {
         var ans = new List<int>();
         if (s.Length < p.Length)
             return ans;
 
-        // digest pattern
-        var dPattern = new Dictionary<char, int>();
-        // digest current part
-        var dCurrent = new Dictionary<char, int>();
-        foreach (var ch in p) {
-            if (dPattern.ContainsKey(ch)) {
-                dPattern[ch] += 1;
-            } else {
-                dPattern[ch] = 1;
-            }
-        }
-        foreach (var k in dPattern.Keys) {
-            dCurrent[k] = 0;
-        }
+        int[] dPattern = new int[26];
+        var pInds = new List<int>();
+        int[] dCurrent = new int[26];
+        foreach (var ch in p)
+            dPattern[ch - 'a'] += 1;
+        foreach (var ch in s.Substring(0,p.Length))
+            dCurrent[ch - 'a'] += 1;
 
-        foreach (var ch in s.Substring(0,p.Length)) {
-            if (dCurrent.ContainsKey(ch))
-                dCurrent[ch] += 1;
-        }
-
-        for (int i = 0; i+p.Length-1 < s.Length; ++i) {
+        for (int i = 0; i < 26; ++i)
+            if (dPattern[i] > 0)
+                pInds.Add(i);
+        
+        for (int i = 0; true; ++i) {
             // INVARANT: dCurrent should be sync-ed at this point.
-            if (Matches(dCurrent, dPattern)) {
+            bool matched = true;
+            for (int j = 0; j < pInds.Count() && matched; ++j)
+                if (dCurrent[pInds[j]] != dPattern[pInds[j]])
+                    matched = false;
+            if (matched) {
                 ans.Add(i);
             }
-            // sliding window, but only focus on pattern chars
             if (i+p.Length < s.Length) {
                 var oldCh = s[i];
                 var newCh = s[i+p.Length];
-                if (oldCh == newCh)
-                    continue;
-                if (dCurrent.ContainsKey(oldCh))
-                    dCurrent[oldCh] -= 1;
-                if (dCurrent.ContainsKey(newCh))
-                    dCurrent[newCh] += 1;
+                if (oldCh != newCh) {
+                    dCurrent[oldCh - 'a'] -= 1;
+                    dCurrent[newCh - 'a'] += 1;
+                }
+            } else {
+                break;
             }
         }
-
         return ans;
     }
 
