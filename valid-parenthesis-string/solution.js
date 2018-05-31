2f9f7542a7cbd4c9d5ead101d5cd9e886b087c72
Javran Cheng <Javran.c@gmail.com>
2018-05-31 00:18:43 -0400

    sol: valid-parenthesis-string

---
+++ valid-parenthesis-string/solution.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = xs => {
+  // the "unbalance" is the number of unbalanced left-parentheses.
+  // we just need to maintain a non-deterministic state because
+  // of existence of the wildcard '*'
+  let unbalanceSet = new Set([0])
+  const next = (unbalance, ch, curSet) => {
+  }
+
+  for (let i = 0; i < xs.length; ++i) {
+    const ch = xs[i]
+    const nextSet = new Set()
+    unbalanceSet.forEach(unbalance => {
+      if (ch === '(' || ch === '*') {
+        nextSet.add(unbalance+1)
+      }
+      if ((ch === ')' || ch === '*') && unbalance >= 1) {
+        nextSet.add(unbalance-1)
+      }
+      if (ch === '*')
+        nextSet.add(unbalance)
+    })
+    unbalanceSet = nextSet
+  }
+
+  return unbalanceSet.has(0)
+}
+
+const test = (inp, exp) =>
+  console.assert(checkValidString(inp) === exp)
+
+test('()', true)
+test('())', false)
+test('(()', false)
+test('((*)', true)
