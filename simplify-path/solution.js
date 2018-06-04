27a3ac6450286bde23f1b32b3ee018023b46b5d6
Javran Cheng <Javran.c@gmail.com>
2018-06-03 23:28:08 -0400

    explained

--- simplify-path/solution.js
+++ simplify-path/solution.js
@@ -3,10 +3,22 @@
  * @return {string}
  */
 const simplifyPath = pathRaw => {
+  /*
+     rather than considering how to simplify a path,
+     why don't we just interpret the raw path as a sequence of commands
+     separated by "/":
+
+     - command ".." goes up a directory (if it's possible)
+     - command "." does nothing
+     - all other commands lead us a layer deeper
+
+     by simluating a stack of sub-dirs (ys below) we can always keep track of this
+   */
   const xs = pathRaw.split(/\/+/).filter(x => x !== '')
   const ys = []
   xs.map(x => {
     if (x === "..") {
+      // JS's pop is already taken care of stack underflow
       ys.pop()
     } else if (x === ".") {
       // NOOP
