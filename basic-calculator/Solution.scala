4f58f505177a6d881d7e8759a14b8724e2fe336b
Javran Cheng <Javran.c@gmail.com>
2018-06-05 21:37:39 -0400

    sol.
    - need to workaround stack issue.

--- basic-calculator/Solution.scala
+++ basic-calculator/Solution.scala
@@ -1,3 +1,18 @@
+import scala.collection.mutable.ArrayBuffer
+
+/*
+ 
+ the difficulty is in determining the appropriate time to perform an arithmetic operation:
+ given "a - (b + c)", the "+" must be processed first before "-" can be done.
+ this is done by keeping info on two stacks, one for storing numbers and another
+ for operations with their priority.
+
+ one we have this representation ("Machine" case class below),
+ we gradually process the input (by "interpret") and
+ try to perform as many operations as possible until
+ we are unsure about priority (by "force").
+
+ */
 object Solution {
   type Token = Either[Char, Int]
 
@@ -8,18 +23,28 @@ object Solution {
     curPriority: Int
   )
 
-  def parse(raw: List[Char]): List[Token] = raw match {
-    case Nil => Nil
-    case x :: xs =>
-      x match {
-        case _ if x.isSpaceChar =>
-          parse(xs.dropWhile(_.isSpaceChar))
-        case '(' | ')' | '+' | '-' =>
-          Left(x) :: parse(xs)
-        case _ if x.isDigit =>
-          val (numsRaw, ys) = raw.span(_.isDigit)
-          Right(numsRaw.mkString("").toInt) :: parse(ys)
+  def parse(raw: List[Char]): List[Token] = {
+    val buf = new ArrayBuffer[Token]()
+    var inp = raw
+    while (inp != Nil) {
+      inp match {
+        case Nil =>
+          return buf.toList
+        case x :: xs =>
+          x match {
+            case _ if x.isSpaceChar =>
+              inp = xs.dropWhile(_.isSpaceChar)
+            case '(' | ')' | '+' | '-' =>
+              buf += Left(x)
+              inp = xs
+            case _ if x.isDigit =>
+              val (numsRaw, ys) = inp.span(_.isDigit)
+              buf += Right(numsRaw.mkString("").toInt)
+              inp = ys
+          }
       }
+    }
+    return buf.toList
   }
 
   // force a machine to run until all ops with priority >= given priority is done
@@ -65,9 +90,9 @@ object Solution {
   }
 
   def main(args: Array[String]): Unit = {
-    println(calculate("(1+(4+5+2)-3)+(6+8)"))
-    println(calculate("3-(13-21)+5"))
-    println(calculate("1 +   1  "))
-    println(calculate("2 - 1 +   2 "))
+    println(calculate("(1+(4+5+2)-3)+(6+8)") == 23)
+    println(calculate("3-(13-21)+5") == 16)
+    println(calculate("1 +   1  ") == 2)
+    println(calculate("2 - 1 +   2 ") == 3)
   }
 }
