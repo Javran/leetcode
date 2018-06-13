20da20b915d48000e5ea617023948a5ea9fae5c8
Javran Cheng <Javran.c@gmail.com>
2018-06-12 22:53:03 -0400

    optimize

--- valid-word-abbreviation/solution.js
+++ valid-word-abbreviation/solution.js
@@ -1,5 +1,6 @@
-const digits = new Set('0123456789')
-const isDigit = x => digits.has(x)
+const digitsTmp = new Array(256)
+'0123456789'.split('').map(ch => { digitsTmp[ch.codePointAt(0)] = true })
+const isDigit = x => Boolean(digitsTmp[x.codePointAt(0)])
 
 /**
  * @param {string} word
@@ -7,8 +8,22 @@ const isDigit = x => digits.has(x)
  * @return {boolean}
  */
 const validWordAbbreviation = (word, abbrRaw) => {
+  // match string
+  let startInd = 0
+  const tryMatch = abbr => {
+    if (typeof abbr === 'number') {
+      if (startInd + abbr - 1 >= word.length)
+        return false
+      startInd += abbr
+    } else {
+      if (word.substr(startInd, abbr.length) !== abbr)
+        return false
+      startInd += abbr.length
+    }
+    return true
+  }
+
   // convert abbr into an array of either strings or numbers.
-  let abbrList = []
   for (let i = 0; i < abbrRaw.length; /* NOOP */) {
     const ch = abbrRaw[i]
     if (isDigit(ch)) {
@@ -20,30 +35,17 @@ const validWordAbbreviation = (word, abbrRaw) => {
       // invalid abbr because the digit starts with 0
       if (d[0] === '0')
         return false
-      abbrList.push(Number(d))
+      if (!tryMatch(Number(d)))
+        return false
       i = j+1
     } else {
       let j = i
       while (j+1 < abbrRaw.length && !isDigit(abbrRaw[j+1]))
              ++j
       const w = abbrRaw.slice(i,j+1)
-      abbrList.push(w)
+      if (!tryMatch(w))
+        return false
       i = j+1
-    }
-  }
-
-  // match string
-  let startInd = 0
-  for (let i = 0; i < abbrList.length; ++i) {
-    const abbr = abbrList[i]
-    if (typeof abbr === 'number') {
-      if (startInd + abbr - 1 >= word.length)
-        return false
-      startInd += abbr
-    } else {
-      if (word.substr(startInd, abbr.length) !== abbr)
-        return false
-      startInd += abbr.length
     }
   }
   return startInd === word.length
