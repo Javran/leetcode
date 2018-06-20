7ea2273cff2cf99207fb19024e1c1707c98c4fb2
Javran Cheng <Javran.c@gmail.com>
2018-06-20 00:27:49 -0400

    sol.

---
+++ edit-distance/solution.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+const minDistance = (word1, word2) => {
+  /*
+     let dist[i][j] be the edit distance between
+     string word1[0..i) and word2[0..j) (using positional index)
+
+     - dist[0][j] = j (delete all chars)
+     - dist[i][0] = i (insert all chars)
+     - dist[i][j] = min of:
+       + dist[i-1][j] + 1 (delete char)
+       + dist[i][j-1] + 1 (insert char)
+       + dist[i-1][j-1] (if word1[i-1] == word2[j-1])
+         or dist[i-1][j-1] + 1 (otherwise we need to replace)
+
+   */
+  const m = word1.length
+  const n = word2.length
+  const dist = new Array(m+1)
+  for (let i = 0; i <= m; ++i)
+    dist[i] = new Uint16Array(n+1)
+  for (let i = 0; i <= m; ++i)
+    dist[i][0] = i
+  for (let j = 1; j <= n; ++j)
+    dist[0][j] = j
+  for (let i = 1; i <= m; ++i)
+    for (let j = 1; j <= n; ++j) {
+      let min = Math.min(dist[i-1][j], dist[i][j-1]) + 1
+      let replaceOrKeep =
+        (word1[i-1] === word2[j-1]) ? dist[i-1][j-1] : dist[i-1][j-1] + 1
+      min = Math.min(min, replaceOrKeep)
+      dist[i][j] = min
+    }
+  return dist[m][n]
+}
+
+console.log(minDistance("horse", "ros"))
+console.log(minDistance("intention", "execution"))
