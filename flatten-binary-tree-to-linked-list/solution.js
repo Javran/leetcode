b21eb645be728af179ecf52ce4f861f06223f0ff
Javran Cheng <Javran.c@gmail.com>
2018-06-20 00:45:14 -0400

    sol.

---
+++ flatten-binary-tree-to-linked-list/solution.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+const flatten = root => {
+  if (!root)
+    return
+  const dummy = new TreeNode(null)
+  let cur = dummy
+  const go = root => {
+    if (root) {
+      const l = root.left, r = root.right
+      cur.right = root
+      root.left = null, root.right = null
+      cur = root
+      go(l)
+      go(r)
+    }
+  }
+  go(root)
+  root.left = null
+  // 1st value is the root itself.
+  root.right = dummy.right.right
+  return
+}
