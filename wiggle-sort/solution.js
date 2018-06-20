16257b1643d03e09b8905ea060b6f211a5f8f815
Javran Cheng <Javran.c@gmail.com>
2018-06-20 01:52:36 -0400

    comment.

--- wiggle-sort/solution.js
+++ wiggle-sort/solution.js
@@ -5,6 +5,16 @@
 const wiggleSort = xs => {
   if (!Array.isArray(xs) || xs.length <= 1)
     return
+  /*
+     the observation is that we can always flip last element with the incoming one
+     if the wiggle-ness is not met:
+     - for wiggle-sorted xs[..i-1], if we want xs[i-1] <= xs[i], but have xs[i-1] > xs[i]
+       swapping xs[i-1] with xs[i] should do the trick,
+       as we have xs[i-2] >= xs[i-1] > xs[i], the swap won't
+       break the relation between xs[i-2] and xs[i-1] but how we have wiggle-sorted
+       xs[..i]
+     - similar for the other case.
+   */
 
   for (let i = 1; i < xs.length; ++i) {
     const swap = () => {
@@ -12,6 +22,7 @@ const wiggleSort = xs => {
       xs[i] = xs[i-1]
       xs[i-1] = tmp
     }
+    // here we just want to alternate between two conditions
     if (i & 1) {
       if (xs[i-1] > xs[i])
         swap()
