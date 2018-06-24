760b85dd3467f527baf69bbee092f500dab50794
Javran Cheng <Javran.c@gmail.com>
2018-06-24 00:02:52 -0400

    cmt.

--- detect-capital/solution.js
+++ detect-capital/solution.js
@@ -3,6 +3,11 @@
  * @return {boolean}
  */
 const detectCapitalUse = word => {
+  /* note that rule can be re-stated in this way:
+     - if we have less than 2 chars, always proper capital.
+     - if we have every char except first one being lower case, it's proper.
+     - otherwise this has to be a word consisting of all upper case chars.
+   */
   if (word.length <= 1)
     return true
   const tl = word.substr(1)
