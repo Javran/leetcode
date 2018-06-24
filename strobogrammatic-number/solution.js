ace38b7e1429428bfee07987ab7d8f3816631a87
Javran Cheng <Javran.c@gmail.com>
2018-06-23 23:38:04 -0400

    cmt.

--- strobogrammatic-number/solution.js
+++ strobogrammatic-number/solution.js
@@ -14,8 +14,10 @@ const isStrobogrammatic = num => {
   const num2 = new Array(num.length)
   for (let i = 0; i < num.length; ++i) {
     const n = num[i]
+    // all nums can be turned upside down (meaningfully)
     if (strobMaps.has(n)) {
       const v = strobMaps.get(n)
+      // reversing
       num2[num.length-i] = v
     } else {
       return false
