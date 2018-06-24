4f39c174fc0bff241f3bc28b52856b4da6945202
Javran Cheng <Javran.c@gmail.com>
2018-06-23 23:36:45 -0400

    sol.

---
+++ strobogrammatic-number/solution.js
@@ -0,0 +1,25 @@
+const strobMaps = new Map([
+  ["0", "0"],
+  ["1", "1"],
+  ["6", "9"],
+  ["8", "8"],
+  ["9", "6"],
+])
+
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+const isStrobogrammatic = num => {
+  const num2 = new Array(num.length)
+  for (let i = 0; i < num.length; ++i) {
+    const n = num[i]
+    if (strobMaps.has(n)) {
+      const v = strobMaps.get(n)
+      num2[num.length-i] = v
+    } else {
+      return false
+    }
+  }
+  return num2.join("") === num
+}
