e97a9ebc958ea8457a0ddd5e37587cab16c80d76
Javran Cheng <Javran.c@gmail.com>
2018-06-24 23:44:42 -0400

    sol.

---
+++ sentence-similarity/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @param {string[][]} pairs
+ * @return {boolean}
+ */
+const areSentencesSimilar = (words1, words2, pairs) => {
+  if (words1.length !== words2.length)
+    return false
+  const n = words1.length
+  const simMap = new Map()
+  pairs.forEach(([u,v]) => {
+    if (!simMap.has(u))
+      simMap.set(u, new Set())
+    if (!simMap.has(v))
+      simMap.set(v, new Set())
+    simMap.get(u).add(v)
+    simMap.get(v).add(u)
+  })
+  for (let i = 0; i < n; ++i) {
+    const w1 = words1[i], w2 = words2[i]
+    if (!(
+      w1 === w2 ||
+      (simMap.has(w1) && simMap.get(w1).has(w2))
+    ))
+      return false
+  }
+  return true
+}
