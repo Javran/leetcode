0a257b1dd9c2578a22d9a3d3d6b74542bf55077d
Javran Cheng <Javran.c@gmail.com>
2018-07-02 23:10:44 -0400

    cmt.

--- max-chunks-to-make-sorted/solution.js
+++ max-chunks-to-make-sorted/solution.js
@@ -10,6 +10,11 @@ const maxChunksToSorted = arr => {
   let ans = 0
   for (let startInd = 0; startInd < arr.length; /* NOOP */) {
     let endInd = startInd
+    // we want to make the minimum chunk in which every element
+    // of the chunk can find its final place after sorting,
+    // this is done by expanding endInd.
+    // note that there's no need of expanding startInd to its left side,
+    // as processed elements should have get their indices covered already.
     for (let i = startInd; i <= endInd; ++i) {
       if (revInd[i] > endInd)
         endInd = revInd[i]
