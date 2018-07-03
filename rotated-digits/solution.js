b8d1836a191e09daa5c37a424693f2bf73bd8165
Javran Cheng <Javran.c@gmail.com>
2018-07-02 20:54:27 -0400

    cmt.

--- rotated-digits/solution.js
+++ rotated-digits/solution.js
@@ -1,15 +1,9 @@
 // it's only rotate-able if we use this fixed set of digits
 // so that's what we gonna do.
-const rDigits = [0,1,2,5,6,8,9]
-// would using a digit (rotate-able) change the number?
-const afterRotate = new Array(10)
-afterRotate[0] = false
-afterRotate[1] = false
-afterRotate[2] = true
-afterRotate[5] = true
-afterRotate[6] = true
-afterRotate[8] = false
-afterRotate[9] = true
+const rDigits = Int8Array.from([0,1,2,5,6,8,9])
+const changed = new Int8Array(10)
+changed[2] = 1, changed[5] = 1
+changed[6] = 1, changed[9] = 1
 
 /**
  * @param {number} N
@@ -17,6 +11,8 @@ afterRotate[9] = true
  */
 const rotatedDigits = N => {
   // just having fun overcomplicating stuff...
+  // well, it could be faster if we don't enumerate only possible solutions
+  // and just go from 1 all the way to N.
   if (N <= 1)
     return 0
   let ans = 0
@@ -33,25 +29,19 @@ const rotatedDigits = N => {
       while (digits[i] === 0 && i+1 < 5)
         ++i
       if (digits[i] !== 0) {
-        // got non-all-zero solution, checking range [i .. 4]
-        const ds = digits.slice(i,5)
-        let different = false
-        for (let ind = i; ind < 5; ++ind) {
-          different = different || afterRotate[digits[ind]]
-        }
-        if (different)
+        if (
+          digits.slice(i,5).some(d => changed[d])
+        )
           ++ans
       }
       return
     }
     for (let i = 0; i < rDigits.length && !stop; ++i) {
-      const d = rDigits[i]
-      digits[dep] = d
+      digits[dep] = rDigits[i]
       go(dep+1)
     }
   }
   go(0)
-
   return ans
 }
 
