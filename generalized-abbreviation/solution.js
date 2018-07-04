9f7e4fed1bbdef0b6b868b0441b1f5b2f5e0685e
Javran Cheng <Javran.c@gmail.com>
2018-07-03 22:15:55 -0400

    a bit improved

--- generalized-abbreviation/solution.js
+++ generalized-abbreviation/solution.js
@@ -5,28 +5,32 @@ const isDigitCode = code => code >= 48 && code <= 57
  * @return {string[]}
  */
 const generateAbbreviations = word => {
+  // not in the way of fast, but it works
   const gen = w => {
     if (w.length === 0)
       return ['']
-    const ans = []
-    ans.push(w)
-    ans.push(String(w.length))
+    const ans = [w, String(w.length)]
     for (let i = 1; i < w.length; ++i) {
       const l = w.substring(0,i)
       const r = w.substring(i)
+      // don't do recursive call, or we end up with more duplicates
       const ls = [l, String(l.length)]
       const rs = gen(r)
       for (let u = 0; u < ls.length; ++u)
         for (let v = 0; v < rs.length; ++v) {
+          if (u === 0 && v === 0)
+            continue
           const x = ls[u], y = rs[v]
+          // note that since we are cutting in the middle
+          // it is guaranteed that both x and y won't be 0
           if (
-            x.length === 0 ||
-            y.length === 0 ||
-            !(isDigitCode(x.codePointAt(x.length-1)) && isDigitCode(y.codePointAt(0)))
+            !(
+              // it's fine to merge when we don't have facing digits
+              isDigitCode(x.codePointAt(x.length-1)) &&
+              isDigitCode(y.codePointAt(0))
+            )
           ) {
-            const w2 = x + y
-            if (w2 !== w)
-              ans.push(w2)
+              ans.push(x+y)
           }
         }
     }
