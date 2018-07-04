06b040e8472f20b35940899a1a1142b935126c69
Javran Cheng <Javran.c@gmail.com>
2018-07-03 23:00:33 -0400

    imp.

--- generalized-abbreviation/solution.js
+++ generalized-abbreviation/solution.js
@@ -1,44 +1,38 @@
-const isDigitCode = code => code >= 48 && code <= 57
-
 /**
  * @param {string} word
  * @return {string[]}
  */
 const generateAbbreviations = word => {
-  // not in the way of fast, but it works
-  const gen = w => {
-    if (w.length === 0)
-      return ['']
-    const ans = [w, String(w.length)]
-    for (let i = 1; i < w.length; ++i) {
-      const l = w.substring(0,i)
-      const r = w.substring(i)
-      // don't do recursive call, or we end up with more duplicates
-      const ls = [l, String(l.length)]
-      const rs = gen(r)
-      for (let u = 0; u < ls.length; ++u)
-        for (let v = 0; v < rs.length; ++v) {
-          if (u === 0 && v === 0)
-            continue
-          const x = ls[u], y = rs[v]
-          // note that since we are cutting in the middle
-          // it is guaranteed that both x and y won't be 0
-          if (
-            !(
-              // it's fine to merge when we don't have facing digits
-              isDigitCode(x.codePointAt(x.length-1)) &&
-              isDigitCode(y.codePointAt(0))
-            )
-          ) {
-              ans.push(x+y)
-          }
-        }
+  if (word === '')
+    return ['']
+
+  const ans = []
+  const groups = new Array(word.length)
+  // split word into non-empty groups
+  const doGroup = (startInd, dep) => {
+    if (startInd === word.length) {
+      // once we have all groups, the word-number has to alternate
+      // to avoid duplication.
+      // and we have exactly two ways of doing so.
+      const xs = groups.slice(0, dep)
+      ans.push(xs.map((x, ind) =>
+        (ind & 1) ? x : String(x.length)).join('')
+      )
+      ans.push(xs.map((x, ind) =>
+        (ind & 1) ? String(x.length) : x).join('')
+      )
+      return
     }
-    return ans
+    for (let i = startInd; i < word.length; ++i) {
+      groups[dep] = word.substring(startInd, i+1)
+      doGroup(i+1, dep+1)
+    }
   }
 
-  return Array.from(new Set(gen(word)))
+  doGroup(0,0)
+  return ans
 }
 
+console.log(generateAbbreviations("w"))
 console.log(generateAbbreviations("word"))
 console.log(generateAbbreviations("worfasdfd"))
