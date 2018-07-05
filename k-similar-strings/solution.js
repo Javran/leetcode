f5770224956bcac6a133395f1df6317530625a5f
Javran Cheng <Javran.c@gmail.com>
2018-07-05 19:05:17 -0400

    sol.

--- k-similar-strings/solution.js
+++ k-similar-strings/solution.js
@@ -11,21 +11,28 @@ function ListNode(str, step, minInd) {
  * @return {number}
  */
 const kSimilarity = (src, tgt) => {
+  // idea: just optimize the heck out of this.
   let qHead = new ListNode(src, 0, 0)
   let qTail = qHead
   let vDiff = null
   let visited = new Set()
   visited.add(src)
   while (qHead !== null) {
+    // minInd limits the lower bound of the index that we are allowed to swap
+    // the observation is that every mismatching char needs swapping
+    // and we don't care when should the swapping happen
     let {str, step, minInd} = qHead
     const diffLocs = []
     for (let i = 0; i < src.length; ++i) {
-      if (str[i] !== tgt[i])
+      if (str.codePointAt(i) !== tgt.codePointAt(i))
         diffLocs.push(i)
     }
     if (diffLocs.length === 0)
       return step
-    // all locations that have difference
+    // vDiff: number of differences
+    // as we search, the difference between current str and tgt
+    // goes down, which means we no longer need to store
+    // "visited" info for strings that have larger difference
     if (vDiff === null) {
       vDiff = diffLocs.length
     } else if (vDiff > diffLocs.length) {
@@ -39,14 +46,15 @@ const kSimilarity = (src, tgt) => {
     const lInd = diffLocs[i]
     for (let j = i+1; j < diffLocs.length; ++j) {
       const rInd = diffLocs[j]
-      if (str[lInd] === str[rInd])
+      if (str.codePointAt(lInd) === str.codePointAt(rInd))
         continue
-
+      const eq1 = str.codePointAt(rInd) === tgt.codePointAt(lInd)
+      const eq2 = str.codePointAt(lInd) === tgt.codePointAt(rInd)
       // see if swapping diffLocs[i], diffLocs[j] can be any better
       if (
-        str[rInd] === tgt[lInd] ||
-        str[lInd] === tgt[rInd]
+        eq1 || eq2
       ) {
+        let curDiff = diffLocs.length - (eq1 ? 1 : 0) - (eq2 ? 1 : 0)
         let newStr =
           [
             str.substring(0, lInd),
@@ -55,7 +63,10 @@ const kSimilarity = (src, tgt) => {
             str[lInd],
             str.substring(rInd+1)
           ].join('')
-        if (!visited.has(newStr)) {
+        if (
+          (!visited.has(newStr)) &&
+          (vDiff === null || curDiff <= vDiff)
+        ) {
           qTail.next = new ListNode(newStr, step, lInd+1)
           qTail = qTail.next
           visited.add(newStr)
@@ -74,7 +85,6 @@ const test = (a,b,c=null) => {
   if (c !== null) {
     console.assert(kSimilarity(a,b) === c)
   }
-
 }
 
 test("abac", "baca", 2)
