4ea5027f160a5326cb890fc36c997dff3875f3b9
Javran Cheng <Javran.c@gmail.com>
2018-07-05 18:48:12 -0400

    opt1

--- k-similar-strings/solution.js
+++ k-similar-strings/solution.js
@@ -12,6 +12,12 @@ const pack = str => {
   return xs
 }
 
+const strEncode = xs =>
+  String.fromCodePoint.apply(
+    null,
+    xs.map(x => x + 97)
+  )
+
 /**
  * @param {string} A
  * @param {string} B
@@ -19,13 +25,13 @@ const pack = str => {
  */
 const kSimilarity = (srcInp, tgtInp) => {
   const src = pack(srcInp), tgt = pack(tgtInp)
-  let vDiff = null
-  let visited = new Set()
   let qHead = new ListNode(src, 0, 0)
   let qTail = qHead
+  let vDiff = null
+  let visited = new Set()
+  visited.add(strEncode(src))
   while (qHead !== null) {
     let {str, step, minInd} = qHead
-    visited.add(str)
     const diffLocs = []
     for (let i = 0; i < src.length; ++i) {
       if (str[i] !== tgt[i])
@@ -33,7 +39,6 @@ const kSimilarity = (srcInp, tgtInp) => {
     }
     if (diffLocs.length === 0)
       return step
-    ++step
     // all locations that have difference
     if (vDiff === null) {
       vDiff = diffLocs.length
@@ -41,28 +46,29 @@ const kSimilarity = (srcInp, tgtInp) => {
       vDiff = diffLocs.length
       visited = new Set()
     }
-    while (diffLocs.length > 1 && diffLocs[0] < minInd)
-      diffLocs.shift()
+    ++step
+    let i = 0
+    while (i < diffLocs.length && diffLocs[i] < minInd)
+      ++i
+    const lInd = diffLocs[i]
+    for (let j = i+1; j < diffLocs.length; ++j) {
+      const rInd = diffLocs[j]
+      if (str[lInd] === str[rInd])
+        continue
 
-    for (let i = 0; i < diffLocs.length; ++i) {
-      const lInd = diffLocs[i]
-      for (let j = i+1; j < diffLocs.length; ++j) {
-        const rInd = diffLocs[j]
-        if (str[lInd] === str[rInd])
-          continue
-
-        // see if swapping diffLocs[i], diffLocs[j] can be any better
-        if (
-          str[rInd] === tgt[lInd] ||
-          str[lInd] === tgt[rInd]
-        ) {
-          const newStr = Int8Array.from(str)
-          newStr[lInd] = str[rInd]
-          newStr[rInd] = str[lInd]
-          if (!visited.has(newStr)) {
-            qTail.next = new ListNode(newStr, step, lInd+1)
-            qTail = qTail.next
-          }
+      // see if swapping diffLocs[i], diffLocs[j] can be any better
+      if (
+        str[rInd] === tgt[lInd] ||
+        str[lInd] === tgt[rInd]
+      ) {
+        const newStr = Int8Array.from(str)
+        newStr[lInd] = str[rInd]
+        newStr[rInd] = str[lInd]
+        const enc = strEncode(newStr)
+        if (!visited.has(enc)) {
+          qTail.next = new ListNode(newStr, step, lInd+1)
+          qTail = qTail.next
+          visited.add(enc)
         }
       }
     }
@@ -70,6 +76,18 @@ const kSimilarity = (srcInp, tgtInp) => {
   }
 }
 
-console.assert(kSimilarity("abac", "baca") === 2)
-console.assert(kSimilarity("a", "a") === 0)
-console.log(kSimilarity("abccaacceecdeea","bcaacceeccdeaae"))
+const test = (a,b,c=null) => {
+  console.time(`test ${a} ${b}`)
+  const result = kSimilarity(a,b)
+  console.timeEnd(`test ${a} ${b}`)
+  console.log(result)
+  if (c !== null) {
+    console.assert(kSimilarity(a,b) === c)
+  }
+
+}
+
+test("abac", "baca", 2)
+test("a", "a", 0)
+test("abccaacceecdeea","bcaacceeccdeaae", 9)
+test("cdebcdeadedaaaebfbcf","baaddacfedebefdabecc")
