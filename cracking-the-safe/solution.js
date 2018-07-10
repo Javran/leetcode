656d190d8772aa61a698bbec78852dfa8a9f3f00
Javran Cheng <Javran.c@gmail.com>
2018-07-10 11:27:44 -0400

    imp.

--- cracking-the-safe/solution.js
+++ cracking-the-safe/solution.js
@@ -4,35 +4,53 @@
  * @return {string}
  */
 const crackSafe = (n, k) => {
-  const visited = new Set()
+  /*
+     there could be systematic way of doing this,
+     but I'll just assume that a solution can be worked out
+     by starting with all 0s and extend one digit at a time
+     to cover any unvisited combination, which is a scenario for DFS.
+   */
+  const combCount = k ** n
+  const prefixMod = combCount / k
+  // actually just store booleans.
+  const visited = new Int8Array(combCount)
   // expected length
-  const totalLen = (k ** n) - 1 + n
+  const totalLen = combCount - 1 + n
   const cur = []
   let solved = false
   for (let i = 0; i < n; ++i)
     cur.push(0)
-  visited.add(cur.join(''))
-  const search = dep => {
-    // to determine cur[dep]
+  visited[0] = 1
+  const search = (dep, encodedPref) => {
+    /*
+       encodedPref is a representation of the prefix:
+       we have processed cur[0..dep-1]
+
+       and we want to determine cur[dep] by
+       allowing reusing the last n-1 digits of cur[0..dep-1].
+       therefore encodedPref is numeric representation of cur[dep-n..dep-1]
+     */
+
     if (dep === totalLen) {
       solved = true
       return
     }
-    const prefix = cur.slice(dep-n+1, dep)
+    // equivalent to `*10` for decimal and `<<1` for binary
+    const encodedPrefK = encodedPref*k
     for (let i = 0; i < k; ++i) {
-      prefix[n-1] = i
-      const encoded = prefix.join('')
-      if (!visited.has(encoded)) {
-        visited.add(encoded)
+      const encoded = encodedPrefK + i
+      if (!visited[encoded]) {
+        visited[encoded] = 1
         cur[dep] = i
-        search(dep+1)
+        // `% prefixMod` removes the most significant digit
+        search(dep+1, encoded % prefixMod)
         if (solved)
           return
-        visited.delete(encoded)
+        visited[encoded] = 0
       }
     }
   }
-  search(n)
+  search(n, 0)
   return solved ? cur.join('') : "???"
 }
 
