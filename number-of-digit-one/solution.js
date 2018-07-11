8cf30b9634b968cc98743c88a827baed39915997
Javran Cheng <Javran.c@gmail.com>
2018-07-11 12:17:59 -0400

    sol.

---
+++ number-of-digit-one/solution.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const countDigitOne = n => {
+  if (n <= 0)
+    return 0
+  const nStr = String(n)
+  let ans = 0
+  for (
+    let i = 0, prefix = 0, base = 10 ** (nStr.length - 1);
+    i < nStr.length;
+    ++i, base /= 10
+  ) {
+    const d = nStr.codePointAt(i) & 15
+    if (d === 0) {
+      ans += prefix*base
+    } else if (d === 1) {
+      // don't use parseInt, as it won't work on empty string
+      const rhs = Number(nStr.substring(i+1))
+      ans += prefix*base + rhs+1
+    } else {
+      // d >= 2
+      ans += (1+prefix)*base
+    }
+    prefix = prefix*10+d
+  }
+
+  return ans
+}
+
+const testAssert = (i, o = null) => {
+  if (o) {
+    console.assert(countDigitOne(i) === o)
+  } else {
+    console.log(countDigitOne(i))
+  }
+}
+
+testAssert(2345, 1775)
+testAssert(10213, 4360)
+testAssert(111111, 66672)
+testAssert(113410200, 106924743)
