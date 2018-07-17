d999a7bf839f6a5728819077d5dabfa2d3cc4ff2
Javran Cheng <Javran.c@gmail.com>
2018-07-16 22:17:32 -0400

    sol.

---
+++ power-of-four/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+const isPowerOfFour = num => {
+  if (num <= 0)
+    return false
+  /*
+     4^? = num
+     => ? * log2(4) = log2(num)
+     => ? = log2(num) / 2
+
+   */
+  const v = Math.floor(Math.floor(Math.log2(num)) / 2)
+  return 4 ** v === num
+}
