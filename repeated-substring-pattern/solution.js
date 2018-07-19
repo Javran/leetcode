d4fec65eb7845585624b1aadef38fbd46d45154c
Javran Cheng <Javran.c@gmail.com>
2018-07-18 21:20:13 -0400

    cmt.

--- repeated-substring-pattern/solution.js
+++ repeated-substring-pattern/solution.js
@@ -5,6 +5,11 @@
 const repeatedSubstringPattern = s => {
   if (s.length <= 1)
     return false
+  /*
+     idea: some to do with self-similiarity. try KMP table:
+     pi[i] is the largest length of proper suffix of 0..i-1 which is
+     also a prefix of 0..i-1, also define pi[0] = -1 for marking termination.
+   */
 
   const pi = new Int16Array(s.length+1)
   pi[0] = -1
@@ -15,7 +20,14 @@ const repeatedSubstringPattern = s => {
     }
     pi[i] = ++k
   }
+  /*
+     we can find a pattern here: when string repeats itself:
 
+     - cLen = s.length - last indicates the length of the smallest cycle length
+     - last % cLen === 0 and also s.length % cLen === 0
+     - last % cLen === 0 && (s.length/cLen - 1)*cLen === last
+       => (s.length - cLen)/cLen*cLen = last, which holds trivially.
+   */
   const last = pi[s.length]
   const cLen = s.length - last
   return last > 0 && last % cLen === 0
