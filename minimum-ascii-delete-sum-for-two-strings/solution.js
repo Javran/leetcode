fa84727d54663162b60c2ee383fcf0019746dfdf
Javran Cheng <Javran.c@gmail.com>
2018-07-19 23:41:16 -0400

    opt.

--- minimum-ascii-delete-sum-for-two-strings/solution.js
+++ minimum-ascii-delete-sum-for-two-strings/solution.js
@@ -4,22 +4,49 @@
  * @return {number}
  */
 const minimumDeleteSum = (s1, s2) => {
-  const f = new Array(s1.length + 1)
-  for (let i = 0; i <= s1.length; ++i)
-    f[i] = new Uint32Array(s2.length + 1)
-  for (let i = 0; i < s1.length; ++i)
-    f[i+1][0] = f[i][0] + s1.codePointAt(i)
+  if (s2.length < s1.length) {
+    // the problem is symmetric, slightly optimize
+    // by allowing the longer one to be rows in DP.
+    const tmp = s1
+    s1 = s2
+    s2 = tmp
+  }
+  /*
+     idea: similar to classical dynamic programming of LCS:
+
+     let f[i][j] be solution for s1[0..i-1], s2[0..j-1]
+
+     f[i][j] = min of:
+
+     - f[i-1][j] + s1[i]
+     - f[i][j-1] + s2[j]
+     - (only possible when s1[i] === s2[j]) f[i-1][j-1]
+
+     notice that computing f[i][j] only requires f[i-1][...],
+     we can recycle to allow taking only O(n) space.
+   */
+  const f = [
+    new Uint32Array(s2.length + 1),
+    new Uint32Array(s2.length + 1),
+  ]
   for (let j = 0; j < s2.length; ++j)
     f[0][j+1] = f[0][j] + s2.codePointAt(j)
-  for (let i = 1; i <= s1.length; ++i) {
+  for (
+    let i = 1, i0 = 1, i1 = 0;
+    i <= s1.length;
+    ++i, i0 = 1 - i0, i1 = 1 - i1
+  ) {
+    const c1 = s1.codePointAt(i-1)
+    f[i0][0] = f[i1][0] + c1
     for (let j = 1; j <= s2.length; ++j) {
-      let min = Math.min(f[i-1][j] + s1.codePointAt(i-1), f[i][j-1] + s2.codePointAt(j-1))
-      if (s1.codePointAt(i-1) === s2.codePointAt(j-1) && min > f[i-1][j-1])
-        min = f[i-1][j-1]
-      f[i][j] = min
+      const c2 = s2.codePointAt(j-1)
+      let min = Math.min(f[i1][j] + c1, f[i0][j-1] + c2)
+      if (c1 === c2 && min > f[i1][j-1])
+        min = f[i1][j-1]
+      f[i0][j] = min
     }
   }
-  return f[s1.length][s2.length]
+  return f[s1.length & 1][s2.length]
 }
 
 console.log(minimumDeleteSum("delete", "leet"))
