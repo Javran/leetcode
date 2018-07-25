5242de3de62bbe353d7548d4b676ae695f4dff95
Javran Cheng <Javran.c@gmail.com>
2018-07-24 23:36:54 -0400

    cmt. opt.

--- binary-tree-longest-consecutive-sequence/solution.js
+++ binary-tree-longest-consecutive-sequence/solution.js
@@ -3,25 +3,38 @@
  * @return {number}
  */
 const longestConsecutive = root => {
+  /*
+     idea: do DFS, let the recursive function return the longest
+     path starting at root, and let upper level root decide
+     whether it can form a longer path.
+     and also since the return value of the recursive function is
+     not necessary the answer, we keep track of the max value as well.
+   */
   let ans = 0
   const go = root => {
     if (root) {
-      if (root.left === null && root.right === null) {
-        if (ans < 1)
-          ans = 1
-        return 1
+      // cur is suppose to be 1, but we can hold that
+      // until we actually want to return or update ans
+      let cur = 0
+      let nextVal = root.val+1
+      if (root.left) {
+        const lLen = go(root.left)
+        if (
+          root.left.val === nextVal &&
+          cur < lLen
+        )
+          cur = lLen
       }
-      let cur = 1
-      const lLen = go(root.left)
-      if (root.left && root.left.val === root.val + 1) {
-        if (cur < lLen + 1)
-          cur = lLen + 1
+
+      if (root.right) {
+        const rLen = go(root.right)
+        if (
+          root.right.val === nextVal &&
+          cur < rLen
+        )
+          cur = rLen
       }
-      const rLen = go(root.right)
-      if (root.right && root.right.val === root.val + 1) {
-        if (cur < rLen + 1)
-          cur = rLen + 1
-      }
+      ++cur
       if (ans < cur)
         ans = cur
       return cur
@@ -36,6 +49,7 @@ const longestConsecutive = root => {
 const {consoleTest, mkTree} =  require('leetcode-zwischenzug')
 const f = consoleTest(longestConsecutive)
 
+f(mkTree([]))(0)
 f(mkTree([1,null,3,2,4,null,null,null,5]))(3)
 f(mkTree([2,null,3,2,null,1]))(2)
 f(mkTree([1,3,2,4,null,3,null,5,5,null,null,null,null,6]))(4)
