ff14e91edf25254fdd6c876bb5328fe2d98c7295
Javran Cheng <Javran.c@gmail.com>
2018-07-30 23:42:16 -0400

    cmt.

--- palindrome-number/solution.js
+++ palindrome-number/solution.js
@@ -3,13 +3,47 @@
  * @return {boolean}
  */
 const isPalindrome = x => {
+  /*
+     idea:
+     this is EXACTLY why you should never try to test a string property
+     without using string unless performance is critical
+     this is just more complicated for nothing.
+   */
   if (x < 0)
     return false
-  if (x === 0)
+  if (x < 10)
     return true
-  const s = String(x)
-  for (let i = 0, j = s.length - 1; i < j; ++i, --j)
-    if (s.codePointAt(i) !== s.codePointAt(j))
+  let num = x
+  let base = 10 ** Math.floor(Math.log10(x))
+  for (
+    /* NOOP */;
+    base >= 100;
+    /* NOOP */
+  ) {
+    const head = Math.floor(num / base)
+    const last = num % 10
+    if (head !== last)
       return false
-  return true
+    num = Math.floor((num - head * base) / 10)
+    base /= 100
+  }
+  if (base === 10) {
+    return Math.floor(num / 10) === num % 10
+  } else {
+    return true
+  }
 }
+
+const {consoleTest} = require('leetcode-zwischenzug')
+const f = consoleTest(isPalindrome)
+
+f(0)(true)
+f(10)(false)
+f(1551)(true)
+f(1552)(false)
+f(1234321)(true)
+f(1000001)(true)
+f(99999)(true)
+f(99)(true)
+f(2)(true)
+f(1000030001)(false)
