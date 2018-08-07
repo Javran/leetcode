7d456b9cda85ac9d715a089a196a47189b53b95e
Javran Cheng <Javran.c@gmail.com>
2018-08-06 23:08:24 -0400

    cmt.

--- shortest-palindrome/solution.js
+++ shortest-palindrome/solution.js
@@ -3,8 +3,23 @@
  * @return {string}
  */
 const shortestPalindrome = s => {
+  /*
+     credit to hpplayer about the idea of using KMP.
+     ref: https://leetcode.com/problems/shortest-palindrome/discuss/60113/Clean-KMP-solution-with-super-detailed-explanation
+
+     idea: since we are only allowed to add characters in front of the original string,
+     the best thing we can do is to identify the longest prefix string that is already
+     a palindrome, and take remaining chars, reverse them and prepend.
+
+     so now the question becomes finding an efficient way of identifying
+     the longest prefix string which is also a palindrome, this is where KMP comes into play.
+
+     remember that the pre-processing of KMP gives us info about longest prefix which is also a suffix of the current substring,
+     we can use this property and build "pi" for p = s + '#' + reversed s. and then the last element of pi
+     tells us the longest palindrome.
+   */
   const revS = s.split('').reverse().join('')
-  const sTmp = `${s}##${revS}`
+  const sTmp = `${s}#${revS}`
   const pi = new Int16Array(sTmp.length+1)
   pi[0] = -1
   let k = -1
@@ -14,7 +29,6 @@ const shortestPalindrome = s => {
     }
     pi[i] = ++k
   }
-  // const pLen = s.substring(0, pi[sTmp.length])
   const sPre = s.substring(pi[sTmp.length]).split('').reverse().join('')
   return `${sPre}${s}`
 }
