71cd0a27cc5afa421768f8e417994fd57bb1482a
Javran Cheng <Javran.c@gmail.com>
2018-08-08 23:55:55 -0700

    opt.

--- trim-a-binary-search-tree/solution.js
+++ trim-a-binary-search-tree/solution.js
@@ -7,18 +7,21 @@ const {TreeNode, consoleTest, mkTree} = require('leetcode-zwischenzug')
  * @return {TreeNode}
  */
 const trimBST = (root, L, R) => {
-  if (root === null)
-    return null
-  // val ... [L, R] (outside)
-  if (root.val < L)
-    return trimBST(root.right, L, R)
-  // [L, R] ... val (outside)
-  if (root.val > R)
-    return trimBST(root.left, L, R)
-  // val is within range.
-  root.left = trimBST(root.left, L, root.val)
-  root.right = trimBST(root.right, root.val, R)
-  return root
+  const go = root => {
+    if (root === null)
+      return null
+    // val ... [L, R] (outside)
+    if (root.val < L)
+      return go(root.right)
+    // [L, R] ... val (outside)
+    if (root.val > R)
+      return go(root.left)
+    // val is within range.
+    root.left = go(root.left)
+    root.right = go(root.right)
+    return root
+  }
+  return go(root)
 }
 
 // TODO: bug fix
