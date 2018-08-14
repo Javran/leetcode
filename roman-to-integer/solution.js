06a8a93d865bb47a55d1c6e9783ea48db020fb45
Javran Cheng <Javran.c@gmail.com>
2018-08-13 22:56:48 -0400

    cmt.

--- roman-to-integer/solution.js
+++ roman-to-integer/solution.js
@@ -11,12 +11,21 @@ const romanToInt = s => {
      by "number unit" I mean we should consider things like "IV", "IX" as wholes
      rather than taking the full string into account at once.
 
+     this suggests that we build a parser which looks one position ahead to see if a "I" turns out
+     to be "IV", "IX" or just a regular "I".
+
    */
   const N = s.length
   let i = 0
+  // parse next number until we are done
   const next = () => {
     if (i >= N)
       return null
+    /*
+       you might think the logic looks a bit messy,
+       but I guess this is the best we can do
+       given that roman numeric system is not the most decent one in the world...
+     */
     if (s[i] === 'I') {
       if (s[i+1] === 'V') {
         i += 2
@@ -50,10 +59,12 @@ const romanToInt = s => {
     if (s[i] === 'C') {
       if (s[i+1] === 'D') {
         i += 2
+        // CD
         return 400
       }
       if (s[i+1] === 'M') {
         i += 2
+        // CM
         return 900
       }
       // C
