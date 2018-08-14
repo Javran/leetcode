8a7457afd4f1a3b5981dd535990220bb373d291d
Javran Cheng <Javran.c@gmail.com>
2018-08-13 23:17:36 -0400

    cmt.

--- factorial-trailing-zeroes/solution.js
+++ factorial-trailing-zeroes/solution.js
@@ -3,6 +3,17 @@
  * @return {number}
  */
 const trailingZeroes = n => {
+  /*
+     idea: counting trailing zeros is actually just figuring out
+     how many 10s can we find from factors of n.
+     we do this by count how many 5s are there - this is because the only
+     way to create a 10 is by 2*5, and since we always have more than sufficient
+     2s floating around, we only need to worry about 5s.
+     - so for the first step we'll count products of 5s.
+     - but there are numbers that have more than one factor of 5, for example 25.
+       in this case we'll recursively solve the problem for (n/5) and do the same counting again.
+     - for the rounding method, we'll be using floor as we don't want to count things that aren't in range.
+   */
   let ans = 0
   while (n >= 5) {
     const t = Math.floor(n / 5)
