9befeee0b96176400fdcf06e4c0469fa7cc4e1b4
Javran Cheng <Javran.c@gmail.com>
2018-08-15 22:53:40 -0400

    cmt.

--- binary-tree-level-order-traversal-ii/solution.js
+++ binary-tree-level-order-traversal-ii/solution.js
@@ -3,6 +3,14 @@
  * @return {number[][]}
  */
 const levelOrderBottom = root => {
+  /*
+     idea: do level order from top to bottom first then reverse the result.
+     as at the beginning of the program we don't know the maximum depth,
+     it's easy to do a top-to-bottom level "traversal"
+     (well, not really a traversal in terms of visiting order)
+     but bottom-to-top will be tricky.
+     we resolve this issue by just reverse our results of top-to-bottom one.
+   */
   const ans = []
   const go = (root, dep) => {
     if (root) {
