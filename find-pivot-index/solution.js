f64ad1417cefaabaff9035bc56721d985749b9b1
Javran Cheng <Javran.c@gmail.com>
2018-08-15 23:12:45 -0400

    cmt.

--- find-pivot-index/solution.js
+++ find-pivot-index/solution.js
@@ -10,6 +10,15 @@ const pivotIndex = nums => {
   if (N === 1) {
     return 0
   }
+  /*
+     idea: to find the pivot index we need a way to efficiently
+     find partial sums starting from 0 to i and from i to the end,
+     in which i could be any valid index.
+     and we do this by usiny acc[i] for left partial sums,
+     together with the total sum, we should be able to figure out right partial sums.
+     the remaining part is just scan through all `i`s and and find first one
+     that meets the requirement
+   */
   // acc[i] = nums[0] + nums[1] + ... + nums[i]
   const acc = new Int32Array(N)
   acc[0] = nums[0]
