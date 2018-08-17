b06033ad4f720929a6de925d96030570ef04c9e5
Javran Cheng <Javran.c@gmail.com>
2018-08-16 21:55:32 -0400

    sol.

---
+++ valid-anagram/solution.js
@@ -0,0 +1,29 @@
+const codeA = 'a'.codePointAt(0)
+
+const freqCount = s => {
+  const freqs = new Uint16Array(26)
+  for (let i = 0; i < s.length; ++i)
+    ++freqs[s.codePointAt(i) - codeA]
+  return freqs
+}
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = (s, t) => {
+  if (s.length !== t.length)
+    return false
+  const xs = freqCount(s)
+  const ys = freqCount(t)
+  return xs.every((x, ind) => x === ys[ind])
+}
+
+const {consoleTest} = require('leetcode-zwischenzug')
+const f = consoleTest(isAnagram)
+
+f("anagram", "nagaram")(true)
+f("aaa", "aaa")(true)
+f("aba", "bba")(false)
+f("rat", "car")(false)
