4436c03ab843a771b04b123e289ceb2f2f4d4939
Javran Cheng <Javran.c@gmail.com>
2018-08-19 00:39:15 -0400

    sol.

---
+++ maximum-average-subarray-i/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findMaxAverage = (xs, k) => {
+  let curSum = 0
+  for (let i = 0; i < k; ++i)
+    curSum += xs[i]
+  let curMax = curSum
+  for (let i = k, j = 0; i < xs.length; ++i, ++j) {
+    curSum = curSum - xs[j] + xs[i]
+    if (curSum > curMax)
+      curMax = curSum
+  }
+  return curMax / k
+}
+
+const {consoleTest} = require('leetcode-zwischenzug')
+const f = consoleTest(findMaxAverage)
+f([1,12,-5,-6,50,3], 4)(12.75)
+f([1,2],2)(3/2)
+f([1,2,3,9],3)((2+3+9)/3)
