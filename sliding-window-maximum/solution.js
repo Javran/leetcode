569cfdc1bddbad618d00a015f451c3e9ff96ef7b
Javran Cheng <Javran.c@gmail.com>
2018-08-26 23:48:20 -0400

    note & opt.

--- sliding-window-maximum/solution.js
+++ sliding-window-maximum/solution.js
@@ -5,38 +5,59 @@
 
    https://leetcode.com/problems/sliding-window-maximum/discuss/65885/This-is-a-typical-monotonic-queue-problem
 
+   my note:
+
+   keep observation: given a window size K, some value will never show up in the result so we might
+   as well in some sense ignore the actual values of them.
+
  */
 
 function Monoqueue() {
+  /*
+     every element of xs is an Array of two values [val, count],
+     which means there are `count` values (that we don't care) in front of `val`.
+
+     `xs` is maintained so that the `val` part is always descending - which means
+     the max value is always the first `val` in the Array
+   */
   this.xs = []
+  this.frontInd = 0
+  this.size = 0
 }
 
 Monoqueue.prototype.push = function(val) {
-  const {xs} = this
+  let {xs, frontInd, size} = this
   let count = 0
-  while (xs.length > 0) {
-    const [x, cnt] = xs[xs.length-1]
+  while (size > 0) {
+    const [x, cnt] = xs[frontInd+size-1]
     if (x < val) {
+      // all `last value` less than `val` can be dropped and represented by
+      // the count number
       count += cnt + 1
-      xs.pop()
+      --size
     } else {
       break
     }
   }
-  xs.push([val, count])
+  xs[frontInd+size] = [val,count]
+  ++size
+  this.size = size
 }
 
 Monoqueue.prototype.pop = function() {
-  const {xs} = this
-  if (xs[0][1] > 0) {
-    --xs[0][1]
+  const {xs, frontInd, size} = this
+  if (xs[frontInd][1] > 0) {
+    // when there are still elements in front
+    --xs[frontInd][1]
   } else {
-    xs.shift()
+    ++this.frontInd
+    --this.size
   }
 }
 
 Monoqueue.prototype.max = function() {
-  return this.xs[0][0]
+  const {xs, frontInd} = this
+  return xs[frontInd][0]
 }
 
 /**
@@ -64,4 +85,4 @@ const maxSlidingWindow = (nums, k) => {
 
 const {consoleTest} = require('leetcode-zwischenzug')
 const f = consoleTest(maxSlidingWindow)
-f([1,3,-1,-3,5,3,6,7], 3)()
+f([1,3,-1,-3,5,3,6,7],3)([3,3,5,5,6,7])
