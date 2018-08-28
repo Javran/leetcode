d30e45579eb950328979ec95148646263db005e0
Javran Cheng <Javran.c@gmail.com>
2018-08-27 21:33:17 -0400

    cmt.

--- shortest-distance-from-all-buildings/solution.js
+++ shortest-distance-from-all-buildings/solution.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 const shortestDistance = grid => {
+  /*
+     idea: BFS with some "pruning"
+
+     (1) notice that if one building cannot reach all other buildings,
+         finding a solution won't be possible.
+     (2) in fact (1) is not always true: if there is only one building,
+         we just want to see if there are empty spaces around it
+         to allow a valid solution, so we treat this case specially.
+     (3) given at least two buildings, we find all coordinates reachable from both
+         and search only in this space
+
+         in fact coordinate of shortest distance can only exist in
+         coordinates reachable from first building.
+         but in case that 4-directions of first building might not
+         necessarily connect with each other, it's important
+         to have a pair of buildings so that we can further narrow down
+         the search space.
+   */
   const rows = grid.length
   const cols = grid[0].length
   // coordinates of buildings
@@ -15,6 +33,17 @@ const shortestDistance = grid => {
     }
   }
   // INVARIANT: input guarantees that bldCoords.length >= 1
+  /*
+     compute a matrix of reachable points for initCoord,
+     isReachable(r,c) is called in addition to checking grids,
+     this allows us to use things like `reach0`, `reach`
+     without changing too much of the code.
+
+     the function returns [reach, emptySpace] in which
+     reach[i][j] is either Infinity or a distance from initCoord,
+     and emptySpace is true iff. there are empty cells encountered
+     throughout the search
+   */
   const computeReach = (initCoord, isReachable) => {
     const [r0,c0] = initCoord
     const reach = new Array(rows)
@@ -63,7 +92,7 @@ const shortestDistance = grid => {
   // if not, we can return -1 right away.
   for (let i = 1; i < bldCoords.length; ++i) {
     const [r,c] = bldCoords[i]
-    if (reach0[r][c] === +Infinity)
+    if (reach0[r][c] === Infinity)
       return -1
   }
 
@@ -71,6 +100,7 @@ const shortestDistance = grid => {
   if (!es1)
     return -1
   // combine the reach of two buildings to build the final "reachable" check.
+  // using both `reachX` allows narrowing down our search space.
   const reach = new Array(rows)
   for (let i = 0; i < rows; ++i) {
     reach[i] = new Uint8Array(cols)
