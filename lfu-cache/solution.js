74979a3a40f59cd4f34fe99a12b404a34d7c1efa
Javran Cheng <Javran.c@gmail.com>
2018-09-03 23:07:03 -0400

    testcases

--- lfu-cache/solution.js
+++ lfu-cache/solution.js
@@ -103,7 +103,6 @@ LFUCache.prototype.put = function(key, value) {
     }
     const oldNode = this.freqList.next.container.next
     const oldKey = oldNode.key
-    console.log(`evict ${oldKey}`)
     this.store.delete(oldKey)
     this.keyToNode.delete(oldKey)
     oldNode.prev.next = oldNode.next
@@ -149,7 +148,6 @@ LFUCache.prototype.debug = function() {
       console.log(`    key: ${cur1.key}`)
     }
   }
-
 }
 
 const {genInt} = require('leetcode-zwischenzug')
@@ -175,35 +173,58 @@ const genTest = () => {
 
 // genTest()
 
-if (true)
-  return true
+const assert = require('assert')
 
-const testImpl = (mkFunc, cmds, args) => {
+const testImpl =
+  (mkFunc, assertEqual = assert.deepStrictEqual) => (cmds, argLists) => expected => {
   const fName = mkFunc.name
   let obj = null
   const ans = []
+
+  console.time(fName)
   for (let i = 0; i < cmds.length; ++i) {
     const cmd = cmds[i]
     if (cmd === fName) {
-      obj = new mkFunc(...args[i])
+      obj = new mkFunc(...argLists[i])
       ans.push(null)
     } else {
-      const ret = obj[cmd].apply(obj, [...args[i]])
+      const ret = obj[cmd].apply(obj, [...argLists[i]])
       ans.push(ret || null)
     }
   }
-  console.log(ans)
+  console.timeEnd(fName)
+  if (typeof expected !== 'undefined') {
+    try {
+      assertEqual(ans, expected)
+    } catch (e) {
+      console.error(`[FAILED]`)
+      console.error('expected:')
+      console.error(JSON.stringify(expected))
+      console.error('actual:')
+      console.error(JSON.stringify(ans))
+      if (assertEqual !== assert.deepStrictEqual) {
+        console.error(`error:`)
+        console.error(e)
+      }
+    }
+  } else {
+    console.log(`Result:`)
+    console.log(JSON.stringify(ans))
+  }
 }
 
-testImpl(
-  LFUCache,
+const f = testImpl(LFUCache)
+
+f(
   ["LFUCache","put","put","get","get","get","put","get","get","put","get","put","get","get","put","get","get"],
   [[3],[5,23],[4,54],[4],[4],[5],[5,60],[3],[4],[3,12],[2],[2,22],[3],[4],[3,37],[2],[3]]
+)(
+  [null,null,null,54,54,23,null,-1,54,null,-1,null,-1,54,null,-1,37]
 )
 
-/*
-["LFUCache","get","get","get","get","get","put","get","get","get","get","put","put","get","get","get","get","get","get","get","get","get","get","get","put","get","get","get","get","get","put","get","get","get","get","put","get","get","get","put","put","put","get","get","put","get","get","get","get","get","get","put","get","put","get","put","get","get","put","get","get","get","get","put","get","put","get","get","get","put","get","get","put","put","get","get","put","get","put","get","get","get","get","get","get","put","get","get","get","get","get","get","get","get","get","get","put","get","put","get","get"]
-[[3],[1],[7],[2],[1],[3],[0,56481],[3],[10],[10],[6],[8,60824],[1,62140],[10],[1],[5],[3],[2],[1],[8],[8],[10],[0],[2],[7,34034],[1],[5],[9],[0],[7],[4,24837],[1],[8],[8],[1],[3,25179],[2],[9],[1],[7,1581],[6,15872],[1,22631],[1],[1],[10,31762],[0],[1],[5],[1],[9],[8],[1,3948],[8],[1,52691],[5],[6,33822],[10],[7],[1,41819],[6],[3],[2],[1],[5,64540],[10],[2,62287],[1],[0],[10],[5,62881],[0],[9],[10,52551],[10,60057],[9],[3],[5,41100],[4],[10,58506],[10],[9],[6],[3],[6],[6],[1,28879],[10],[5],[9],[8],[2],[7],[4],[7],[6],[3],[8,58986],[10],[4,11287],[2],[1]]
-
-[null,-1,-1,-1,-1,-1,null,-1,-1,-1,-1,null,null,-1,62140,-1,-1,-1,62140,60824,60824,-1,56481,-1,null,62140,-1,-1,-1,34034,null,62140,60824,60824,62140,null,-1,-1,62140,null,null,null,22631,22631,null,-1,22631,-1,22631,-1,60824,null,60824,null,-1,null,-1,-1,null,33822,-1,-1,41819,null,-1,null,41819,-1,-1,null,-1,-1,null,null,-1,-1,null,-1,null,58506,-1,-1,-1,-1,-1,null,58506,-1,-1,60824,-1,-1,-1,-1,-1,-1,null,58506,null,-1,28879]
-*/
+f(
+  ["LFUCache","get","get","get","get","get","put","get","get","get","get","put","put","get","get","get","get","get","get","get","get","get","get","get","put","get","get","get","get","get","put","get","get","get","get","put","get","get","get","put","put","put","get","get","put","get","get","get","get","get","get","put","get","put","get","put","get","get","put","get","get","get","get","put","get","put","get","get","get","put","get","get","put","put","get","get","put","get","put","get","get","get","get","get","get","put","get","get","get","get","get","get","get","get","get","get","put","get","put","get","get"],
+  [[3],[1],[7],[2],[1],[3],[0,56481],[3],[10],[10],[6],[8,60824],[1,62140],[10],[1],[5],[3],[2],[1],[8],[8],[10],[0],[2],[7,34034],[1],[5],[9],[0],[7],[4,24837],[1],[8],[8],[1],[3,25179],[2],[9],[1],[7,1581],[6,15872],[1,22631],[1],[1],[10,31762],[0],[1],[5],[1],[9],[8],[1,3948],[8],[1,52691],[5],[6,33822],[10],[7],[1,41819],[6],[3],[2],[1],[5,64540],[10],[2,62287],[1],[0],[10],[5,62881],[0],[9],[10,52551],[10,60057],[9],[3],[5,41100],[4],[10,58506],[10],[9],[6],[3],[6],[6],[1,28879],[10],[5],[9],[8],[2],[7],[4],[7],[6],[3],[8,58986],[10],[4,11287],[2],[1]]
+)(
+  [null,-1,-1,-1,-1,-1,null,-1,-1,-1,-1,null,null,-1,62140,-1,-1,-1,62140,60824,60824,-1,56481,-1,null,62140,-1,-1,-1,34034,null,62140,60824,60824,62140,null,-1,-1,62140,null,null,null,22631,22631,null,-1,22631,-1,22631,-1,60824,null,60824,null,-1,null,-1,-1,null,33822,-1,-1,41819,null,-1,null,41819,-1,-1,null,-1,-1,null,null,-1,-1,null,-1,null,58506,-1,-1,-1,-1,-1,null,58506,-1,-1,60824,-1,-1,-1,-1,-1,-1,null,58506,null,-1,28879]
+)
