7a1d5002b19cc17287dee5649f0d94cb4bec29ff
Javran Cheng <Javran.c@gmail.com>
2018-09-07 22:53:12 -0400

    cmt.

--- number-of-boomerangs/solution.js
+++ number-of-boomerangs/solution.js
@@ -3,7 +3,37 @@
  * @return {number}
  */
 const numberOfBoomerangs = points => {
-  // pMaps[<coord>][<distSq>] = int
+  /*
+     idea:
+
+     to count # of boomerangs, the trick is to count by middle points.
+     imagine that every coordinate on the map has a bucket,
+     so what we do is to take every possible pair of points p1 p2
+     and put them into buckets:
+     for coordinate p1 we put p2 there, and for coord p2 we put p1 there,
+     then as we scan through all buckets, for each bucket on coord c we can identify
+     the group in which all point share the same distance to c,
+     then (size of c) * (size of c-1) is the # of boomerangs we have whose point c
+     is the middle point.
+     note that a normal process of counting pairs would do n*(n-1)/2 for n,
+     but here, since we have ensured that i < j, we rest half of the results are also
+     what we need, so there is no need of doing the division.
+
+     extra optimizations:
+
+     - square root are monotonic, for the purpose of groupping, there is no need
+       of performing the actual square root function.
+     - we don't actually care about the coordinates in the bucket as long as we keep track
+       of the squared distance, that suggests maintaining counters are good enough.
+
+     all of the analysis above lead to pMaps below:
+
+     pMaps[<coord>][<distSq>] = <count>
+
+     means there are <count> boomerangs whose middle point is <coord> and their two legs
+     both have a length of Math.sqrt(<distSq>)
+
+   */
   const toKey = (x,y) => `${x},${y}`
   const pMaps = new Map()
   for (let i = 0; i < points.length; ++i) {
