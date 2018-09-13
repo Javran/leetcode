4b28d9e1a8453ded1e37488e78ef655c0f0dc44c
Javran Cheng <Javran.c@gmail.com>
2018-09-12 22:39:13 -0400

    sol.

---
+++ excel-sheet-column-number/solution.js
@@ -0,0 +1,23 @@
+const codeA = 'A'.codePointAt(0)
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const titleToNumber = s => {
+  /*
+     idea: just need to find the pattern.
+   */
+  let ans = 0
+  for (let base = 1, i = s.length-1; i >= 0; --i, base *= 26) {
+    const code = s.codePointAt(i) - codeA + 1
+    ans += code * base
+  }
+  return ans
+}
+
+const {cTestFunc} = require('leetcode-zwischenzug')
+const f = cTestFunc(titleToNumber)
+f("A")(1)
+f("ZZAZB")(12339706)
+f("ASDFGH")(20638470)
