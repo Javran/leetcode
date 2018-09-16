6fffd465ed85190e469bec7e67f77c35e78e0aba
Javran Cheng <Javran.c@gmail.com>
2018-09-15 22:37:22 -0400

    opt.

--- maximum-product-of-three-numbers/solution.js
+++ maximum-product-of-three-numbers/solution.js
@@ -10,7 +10,29 @@ const maximumProduct = nums => {
      - 1 maximum number and 2 minimum number of the array.
    */
   const N = nums.length
-  nums.sort((x,y) => x-y)
+  // sorting is definiely an overkill as we just need few numbers
+  // from beginning and end of nums.
+  // so here we just perform 3+2 = 5 iterations of bubble sort.
+  for (let i = 0; i < 3; ++i) {
+    for (let j = 1; j < N - i; ++j) {
+      if (nums[j-1] > nums[j]) {
+        const tmp = nums[j]
+        nums[j] = nums[j-1]
+        nums[j-1] = tmp
+      }
+    }
+  }
+  // now last 3 nums are sorted.
+  for (let i = 0; i < 2; ++i) {
+    for (let j = N-4; j > i; --j) {
+      if (nums[j-1] > nums[j]) {
+        const tmp = nums[j]
+        nums[j] = nums[j-1]
+        nums[j-1] = tmp
+      }
+    }
+  }
+  // now first 2 are sorted
   const a = nums[0] * nums[1] * nums[N-1]
   const b = nums[N-3] * nums[N-2] * nums[N-1]
   return a >= b ? a : b
