18b049263bfc4b4d740b0f31420e6091e2504fc0
Javran Cheng <Javran.c@gmail.com>
2018-09-23 23:11:14 -0400

    sol.

---
+++ valid-word-square/solution.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+const validWordSquare = words => {
+  const rows = words.length
+  if (rows === 0)
+    return true
+  let cols = words[0].length
+  for (let i = 1; i < rows; ++i)
+    if (words[i].length > cols)
+      cols = words[i].length
+  if (rows !== cols)
+    return false
+  const N = rows
+  const getCh = (i,j) => {
+    const rowStr = words[i]
+    return j < rowStr.length ? rowStr[j] : null
+  }
+  for (let i = 0; i < N; ++i) {
+    for (let j = i+1; j < N; ++j) {
+      if (getCh(i,j) !== getCh(j,i))
+        return false
+    }
+  }
+  return true
+}
+
+const {cTestFunc} = require('leetcode-zwischenzug')
+const f = cTestFunc(validWordSquare)
+
+f([
+  "abcd",
+  "bnrt",
+  "crmy",
+  "dtye"
+])(true)
+f([
+  "abcd",
+  "bnrt",
+  "crm",
+  "dt"
+])(true)
+f([
+  "ball",
+  "area",
+  "read",
+  "lady"
+])(false)
+
+// to problem setter: flip you
+f(["abc","bde","cefg"])(false)
