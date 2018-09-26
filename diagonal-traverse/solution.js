5a527554f7d506fcab18fb024ce3798dc07983df
Javran Cheng <Javran.c@gmail.com>
2018-09-25 23:07:31 -0400

    cmt.

--- diagonal-traverse/solution.js
+++ diagonal-traverse/solution.js
@@ -3,6 +3,26 @@
  * @return {number[]}
  */
 const findDiagonalOrder = mat => {
+  /*
+     idea:
+
+     first observe:
+
+     (0,0) (0,1) (0,2)
+     (1,0) (1,1) (1,2)
+     (2,0) (2,1) (2,2)
+     (3,0) (3,1) (3,2)
+
+     we can see that if coordinates are denoted (r,c),
+     then r+c is a constant for each diagonal and bumps each time by one:
+
+     r+c === 0: (0,0)
+     r+c === 1: (1,0) (0,1)
+     r+c === 2: (2,0) (1,1) (0,2)
+
+     then it's just a matter of figuring out the expected direction
+     and which coordinate goes first.
+   */
   const rows = mat.length
   if (rows === 0)
     return []
