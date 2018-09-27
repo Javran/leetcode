64b2ead19661c87d1b3b95eead4f8844e21f9f61
Javran Cheng <Javran.c@gmail.com>
2018-09-27 00:03:32 -0400

    sol.

---
+++ minimum-genetic-mutation/solution.js
@@ -0,0 +1,114 @@
+const codeConsts = 'ACGT'
+
+const getInd = (str, i) => {
+  const code = str.codePointAt(i)
+  for (let i = 0; i < codeConsts.length; ++i) {
+    if (codeConsts.codePointAt(i) === code)
+      return i
+  }
+  throw `invalid char: ${str[i]}`
+}
+
+function TrieNode(word) {
+  this.word = word
+  this.children = [null, null, null, null]
+}
+
+function QNode(word, dep) {
+  this.word = word
+  this.dep = dep
+  this.next = null
+}
+
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {string[]} bank
+ * @return {number}
+ */
+const minMutation = (start, end, bank) => {
+  if (start === end)
+    return 0
+  if (!bank.includes(end))
+    return -1
+
+  const tRoot = new TrieNode(null)
+  // assuming word.length > 0
+  const trieInsert = word => {
+    let tCur = tRoot
+    for (let i = 0; i < word.length; ++i) {
+      const gene = getInd(word, i)
+      if (tCur.children[gene] === null) {
+        tCur.children[gene] = new TrieNode(null)
+      }
+      tCur = tCur.children[gene]
+    }
+    tCur.word = word
+  }
+
+  trieInsert(start)
+  for (let i = 0; i < bank.length; ++i)
+    trieInsert(bank[i])
+
+  const nexts = word => {
+    const retSet = new Set()
+    const collect = wildInd => {
+      const collectAux = (tCur, ind) => {
+        if (tCur === null)
+          return
+        if (ind === word.length) {
+          if (tCur.word !== null)
+            retSet.add(tCur.word)
+          return
+        }
+
+        if (ind === wildInd) {
+          for (let j = 0; j < tCur.children.length; ++j) {
+            collectAux(tCur.children[j], ind+1)
+          }
+        } else {
+          const gene = getInd(word, ind)
+          collectAux(tCur.children[gene], ind+1)
+        }
+      }
+      return collectAux
+    }
+
+    for (let i = 0; i < word.length; ++i) {
+      collect(i)(tRoot, 0)
+    }
+    retSet.delete(word)
+    return [...retSet]
+  }
+
+  let qHead = new QNode(start, 0)
+  let qTail = qHead
+  const visited = new Set()
+  visited.add(start)
+
+  while (qHead !== null) {
+    const {word, dep} = qHead
+    if (word === end)
+      return dep
+    const nextWords = nexts(word)
+
+    for (let i = 0; i < nextWords.length; ++i) {
+      const nWord = nextWords[i]
+      if (visited.has(nWord))
+        continue
+      visited.add(nWord)
+      qTail.next = new QNode(nWord, dep+1)
+      qTail = qTail.next
+    }
+
+    qHead = qHead.next
+  }
+
+  return -1
+}
+
+const {cTestFunc} = require('leetcode-zwischenzug')
+const f = cTestFunc(minMutation)
+f("AACCGGTT", "AACCGGTA", ["AACCGGTA"])(1)
+f("AACCGGTT", "AAACGGTA", ["AACCGGTA", "AACCGCTA", "AAACGGTA"])(2)
+f("AAAAACCC", "AACCCCCC", ["AAAACCCC", "AAACCCCC", "AACCCCCC"])(3)
