fe9ff2213628d61b9f4ce0aeb62e4e0b1af4ff7c
Javran Cheng <Javran.c@gmail.com>
2018-10-02 23:46:26 -0400

    sol2.

--- sort-array-by-parity/solution.js
+++ sort-array-by-parity/solution.js
@@ -2,4 +2,20 @@
  * @param {number[]} A
  * @return {number[]}
  */
-const sortArrayByParity = A => A.sort((x,y) => (x & 1) - (y & 1))
+/*
+   idea: we can do a straightforward sorting about parity
+ */
+const sortArrayByParitySlow = A => A.sort((x,y) => (x & 1) - (y & 1))
+
+const sortArrayByParity = A => {
+  /*
+     idea: put them into two buckets about parity
+   */
+  const evens = []
+  const odds = []
+  for (let i = 0; i < A.length; ++i) {
+    const n = A[i];
+    ((n & 1) ? odds : evens).push(n)
+  }
+  return evens.concat(odds)
+}
