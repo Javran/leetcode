6c9cef33f60f17b3255d12e4dd83371ddb6df5a9
Javran Cheng <Javran.c@gmail.com>
2018-10-03 23:58:15 -0400

    cmt.

--- monotonic-array/solution.js
+++ monotonic-array/solution.js
@@ -3,6 +3,15 @@
  * @return {boolean}
  */
 const isMonotonic = xs => {
+  /*
+     idea: we want to identify whether
+     the array number runs in a certain direction:
+
+     - sign === 0: for now the scanned part of the array all have the same value
+     - sign === 1: the scanned part is increasing, so we need rest of
+       the array to be non-decreasing
+     - sign === -1: mirror case of sign === 1
+   */
   if (xs.length <= 2) {
     return true
   }
@@ -10,10 +19,13 @@ const isMonotonic = xs => {
   for (let i = 1; i < xs.length; ++i) {
     const cur = Math.sign(xs[i] - xs[i-1])
     if (sign === 0) {
+      // the array "direction" is not yet determined
       if (cur !== 0) {
         sign = cur
       }
     } else {
+      // the array "direction" is determined,
+      // and we want rest of the array to be consistent with our direction.
       if (cur !== 0 && cur !== sign) {
         return false
       }
