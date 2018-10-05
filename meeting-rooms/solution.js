50792a4d69f2e82556dea453dd33690328ec448c
Javran Cheng <Javran.c@gmail.com>
2018-10-04 23:08:16 -0400

    cmt.

--- meeting-rooms/solution.js
+++ meeting-rooms/solution.js
@@ -3,6 +3,13 @@
  * @return {boolean}
  */
 const canAttendMeetings = ivs => {
+  /*
+     idea: sort by start time, move time forward and see
+     if there are conflictions.
+     because for an Interval, we have start < end,
+     the "end time" will always be pushed forward, allowing conflictions
+     to be discovered.
+   */
   ivs.sort((u,v) => u.start - v.start)
   let endTime = -Infinity
   for (let i = 0; i < ivs.length; ++i) {
