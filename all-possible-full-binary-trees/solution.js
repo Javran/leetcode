caa1145ad2af8619f5f2812515755bbd4c2f226d
Javran Cheng <Javran.c@gmail.com>
2018-10-05 23:37:05 -0400

    cmt.

--- all-possible-full-binary-trees/solution.js
+++ all-possible-full-binary-trees/solution.js
@@ -8,6 +8,12 @@ memo[1] = [new TreeNode(0)]
  * @return {TreeNode[]}
  */
 const allPossibleFBT = N => {
+  /*
+     idea: an ideal situation where memoization comes in handy:
+     knowing N alone allows us to look results up in a table,
+     so we might as well do that to maximize shared structure -
+     given that the result does not suppose to be mutated.
+   */
   if (N in memo)
     return memo[N]
   const ans = []
@@ -18,6 +24,7 @@ const allPossibleFBT = N => {
   ) {
     const lResults = allPossibleFBT(lCount)
     const rResults = allPossibleFBT(rCount)
+    // non-determinism
     lResults.forEach(lT => {
       rResults.forEach(rT => {
         const bt = new TreeNode(0)
