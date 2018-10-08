94f1d2b74a84f3a88418840525cace1fdd1f2585
Javran Cheng <Javran.c@gmail.com>
2018-10-07 23:57:57 -0400

    sol.

--- subdomain-visit-count/solution.js
+++ subdomain-visit-count/solution.js
@@ -14,8 +14,21 @@ const subdomainVisits = cpdomains => {
     const [_ignored, freqRaw, domRaw] = matchResult
     const freq = Number(freqRaw)
     const doms = domRaw.split('.')
+    for (let i = 0; i < doms.length; ++i) {
+      const curDom = doms.slice(i).join('.')
+      if (counts.has(curDom)) {
+        counts.set(curDom, counts.get(curDom) + freq)
+      } else {
+        counts.set(curDom, freq)
+      }
+    }
   }
   cpdomains.forEach(reg)
-
-  return [...counts.entries()]
+  return [...counts.entries()].map(([d,q]) => `${q} ${d}`)
 }
+
+const {cTestFunc} = require('leetcode-zwischenzug')
+const f = cTestFunc(subdomainVisits)
+
+f(["9001 discuss.leetcode.com"])()
+f(["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"])()
