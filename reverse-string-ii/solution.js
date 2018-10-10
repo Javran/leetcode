eb2a3debc5c5071792112c1dbafc5eb7ef1e2e82
Javran Cheng <Javran.c@gmail.com>
2018-10-09 23:41:34 -0400

    cmt.

--- reverse-string-ii/solution.js
+++ reverse-string-ii/solution.js
@@ -9,9 +9,21 @@ const reverseStr = (s, k) => {
      we just need to split the input string into group of k,
      and then reverse every other group.
    */
+
+  /*
+     like "String Builder" or similar concepts in other languages,
+     it helps if we maintain a collection of strings and ask standard lib
+     to concatenate them for us.
+
+     thinks about what happens for ((a+b)+c)+d where a,b,c,d are all strings,
+     things like string builders will avoid creation of intermediate string values
+     therefore the speedup
+   */
   const ansArr = []
   for (let i = 0, rev = true; i < s.length; i += k, rev = !rev) {
+    // current group
     const cur = s.substring(i, i+k)
+    // whether this group needs reversing
     ansArr.push(rev ? cur.split('').reverse().join('') : cur)
   }
   return ansArr.join('')
