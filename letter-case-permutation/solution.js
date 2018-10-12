c78d0c086d2b07d826916e03d1e2762fac03d047
Javran Cheng <Javran.c@gmail.com>
2018-10-11 22:49:19 -0400

    opt.

--- letter-case-permutation/solution.js
+++ letter-case-permutation/solution.js
@@ -2,13 +2,6 @@ const code0 = '0'.codePointAt(0)
 const code9 = '9'.codePointAt(0)
 const codeA = 'A'.codePointAt(0)
 
-const splitCh = code => {
-  if (code >= code0 && code <= code9)
-    return [String.fromCodePoint(code)]
-  const upCode = codeA + (code & 31) -1
-  return [String.fromCodePoint(upCode+32), String.fromCodePoint(upCode)]
-}
-
 /**
  * @param {string} S
  * @return {string[]}
@@ -16,15 +9,30 @@ const splitCh = code => {
 const letterCasePermutation = S => {
   const cur = new Array(S.length)
   const ans = []
+
+  const splitCh = ind => {
+    const code = S.codePointAt(ind)
+    if (code >= code0 && code <= code9)
+      return null
+    return codeA + (code & 31) -1
+  }
+
   const gen = dep => {
     if (dep === S.length) {
       ans.push(cur.join(''))
       return
     }
-    splitCh(S.codePointAt(dep)).forEach(ch => {
-      cur[dep] = ch
+
+    const maySplit = splitCh(dep)
+    if (maySplit === null) {
+      cur[dep] = S[dep]
       gen(dep+1)
-    })
+    } else {
+      cur[dep] = String.fromCodePoint(maySplit+32)
+      gen(dep+1)
+      cur[dep] = String.fromCodePoint(maySplit)
+      gen(dep+1)
+    }
   }
 
   gen(0)
