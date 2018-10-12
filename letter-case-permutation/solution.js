a7e91adcaf06bb41b69f041d2889f8b768c99e3b
Javran Cheng <Javran.c@gmail.com>
2018-10-11 23:06:31 -0400

    cmt.

--- letter-case-permutation/solution.js
+++ letter-case-permutation/solution.js
@@ -3,7 +3,19 @@
  * @return {string[]}
  */
 const letterCasePermutation = S => {
+  /*
+     idea: a recursive solution should do.
+   */
+
+  /*
+     observation: the output value is a set of strings
+     and is irrelevant to original case of input,
+     therefore we may as well "normalize" the input
+     so to reduce the amount of branching during recursion.
+   */
   S = S.toUpperCase()
+
+  // tmp structure for building the string
   const cur = new Array(S.length)
   const ans = []
 
@@ -14,10 +26,12 @@ const letterCasePermutation = S => {
     }
 
     const code = S.codePointAt(dep)
+    // assume that the input only contains 0-9, A-Z, a-z this bound check is safe.
     if (code <= 57 /* '9' */) {
       cur[dep] = String.fromCodePoint(code)
       gen(dep+1)
     } else  {
+      // try both lower and upper, eliminate intermediate need for Arrays
       cur[dep] = String.fromCodePoint(code+32)
       gen(dep+1)
       cur[dep] = String.fromCodePoint(code)
