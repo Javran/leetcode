aae526651b3d428a9da51ea0e1a5aa0484c5574e
Javran Cheng <Javran.c@gmail.com>
2018-10-16 23:18:50 -0400

    cmt.

--- construct-string-from-binary-tree/solution.js
+++ construct-string-from-binary-tree/solution.js
@@ -3,6 +3,18 @@
  * @return {string}
  */
 const tree2str = t => {
+  /*
+     idea: we just need to discuss case-by-case,
+     there is no need of generalizing this as
+     the rules are "biased" in a way that a left-empty-subtree
+     and right one has totally different rules.
+   */
+  /*
+     also note that a property of tree2str is that
+     the result is '' if and only if the input is null,
+     in other words, we know exactly the places where
+     parentheses are or aren't necessary.
+   */
   if (t === null)
     return ''
 
