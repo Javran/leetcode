7e08ed839234ef9125589bb69b104e755fde1d76
Javran Cheng <Javran.c@gmail.com>
2018-10-19 23:45:55 -0400

    cmt.

--- binary-tree-tilt/solution.js
+++ binary-tree-tilt/solution.js
@@ -3,11 +3,21 @@
  * @return {number}
  */
 const findTilt = root => {
+  /*
+     idea: we can have a recursive function
+     to compute sum of each subtree.
+     by doing so, we notice that because
+     the recursive call gives us sum of two subtress of the current node,
+     we have a perfect chance to compute tilt and accumulate that value somewhere.
+   */
   let tSum = 0
+  // returns sum of the given tree
   const go = cur => {
     if (cur !== null) {
       const lSum = go(cur.left)
       const rSum = go(cur.right)
+      // while we have sums of two subtrees,
+      // we might as well compute and accumulate tilt here.
       tSum += Math.abs(lSum - rSum)
       return cur.val + lSum + rSum
     } else {
