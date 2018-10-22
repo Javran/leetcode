8b0d9a14a56ff70988b703af474d115a639903b7
Javran Cheng <Javran.c@gmail.com>
2018-10-21 22:47:07 -0400

    cmt.

--- reshape-the-matrix/solution.js
+++ reshape-the-matrix/solution.js
@@ -5,17 +5,27 @@
  * @return {number[][]}
  */
 const matrixReshape = (nums, rows, cols) => {
+  /*
+     idea: the use of generator gives us a decent and concise solution.
+   */
   const rows0 = nums.length
   if (rows0 === 0)
     return nums
   const cols0 = nums[0].length
+
   if (
+    // since rows and cols are positive,
+    // we know this is impossible to shape
     cols0 === 0 ||
+    // # of elements simply mismatch in this case
     rows0 * cols0 !== rows * cols ||
+    // shapes are the same, nothing to do
     (rows0 === rows && cols0 === cols)
   )
     return nums
 
+  // this generator allows us to keep the process of iterating
+  // isolated and under control.
   function *getNext() {
     for (let i = 0; i < rows0; ++i) {
       for (let j = 0; j < cols0; ++j) {
@@ -30,6 +40,7 @@ const matrixReshape = (nums, rows, cols) => {
   for (let i = 0; i < rows; ++i) {
     ret[i] = new Array(cols)
     for (let j = 0; j < cols; ++j) {
+      // copy to target array by visiting the original one
       ret[i][j] = g.next().value
     }
   }
